dd3db9d0be483e7382953bde1267b5c8c4b5d8b8
Gábor Plesz <gabor.plesz@gmail.com>
2013-05-28 12:19:35 +0200

    Initial commit

---
+++ soMVC/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using MyNamespace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace soMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult MyAction()
+        {
+            var model = new List<MyModel>();
+            for (int i = 0; i < 10; i++)
+            {
+                var item = new MyModel() 
+                    {
+                    Name = string.Format("Name{0}", i),
+                    Use = (i % 2 == 0),
+                    ListAId = null,
+                    ListBId = null,
+                    ListCId = null
+                    };
+
+                for (int j = 0; j < 10; j++)
+			    {
+			        item.ListA.Add( new ListAList() 
+                        {
+                            Id=j,
+                            Name = string.Format("Name {0}-{1}",i,j)
+                        });
+			    }
+                model.Add(item);
+            }
+
+            return View(model);
+        }
+
+        public JsonResult PopulateOption(int? listid, string name)
+        {
+
+            //todo: preparing the data source filter
+
+            var sites = new[]
+            {
+                new { id = "1", name = "Name 1" },
+                new { id = "2", name = "Name 2" },
+                new { id = "3", name = "Name 3" },
+            };
+            return Json(sites, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ soMVC/Models/MyModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace MyNamespace
+{
+    public class MyModel
+    {
+        public MyModel() { ListA = new List<ListAList>(); }
+        public bool Use { get; set; }
+        public string Name { get; set; }
+        public int? ListAId { get; set; }
+        public int? ListBId { get; set; }
+        public int? ListCId { get; set; }
+        public IList<ListAList> ListA { get; set; }
+    }
+
+    public class ListAList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+}
